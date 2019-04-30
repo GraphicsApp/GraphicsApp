ffafcfec9f1ec0f746f04d84ec7afa980ac51be5
Alexander Bazo <alexanderbazo@googlemail.com>
2019-04-30 11:08:23 +0200

    Add core class to split internal and client facing api in GraphicsApp

--- src/de/mi/ur/gpr/app/GraphicsApp.java
+++ src/de/mi/ur/gpr/app/GraphicsApp.java
@@ -6,62 +6,27 @@ import de.mi.ur.gpr.events.GraphicsAppMouseListener;
 import de.mi.ur.gpr.events.KeyPressedEvent;
 import de.mi.ur.gpr.events.MouseClickedEvent;
 import de.mi.ur.gpr.graphics.Background;
-import de.mi.ur.gpr.graphics.GraphicsObject;
 
-import java.util.ArrayList;
+public abstract class GraphicsApp extends GraphicsAppCore implements GraphicsAppKeyListener, GraphicsAppMouseListener {
 
-public abstract class GraphicsApp implements GraphicsAppKeyListener, GraphicsAppMouseListener {
+    private Background background;
 
-    private static GraphicsApp app = null;
-    private Background background = null;
-    private ArrayList<GraphicsObject> objects;
-    private ArrayList<GraphicsObject> drawBuffer;
-    private Config config;
-
-    /*
-     * Full singelton pattern might not be possible:
-     *
-     * On runtime, only one GraphicsApp instance is allowed but all GraphicsApps need a public constructor to be created
-     * in GraphicsAppLauncher via reflection.
-     */
     public GraphicsApp() {
-        try {
-            if (app == null) {
-                app = this;
-                objects = new ArrayList<GraphicsObject>();
-                drawBuffer = new ArrayList<GraphicsObject>();
-            } else {
-                throw new OnlyOneGraphicsAppAllowedException();
-            }
-        } catch (OnlyOneGraphicsAppAllowedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static GraphicsApp getApp() {
-        return app;
-    }
-
-    public void setConfig(Config config) {
-        this.config = config;
+        super();
     }
 
     public abstract void init();
 
-
     public abstract void draw();
 
-    public GraphicsObject[] getObjects() {
-        // TODO: Think about passing copies of the objects!
-        return objects.toArray(new GraphicsObject[objects.size()]);
+    @Override
+    public void onMouseClicked(MouseClickedEvent event) {
+
     }
 
-    public int getWidth() {
-        return config.getWidth();
-    }
+    @Override
+    public void onKeyPressed(KeyPressedEvent event) {
 
-    public int getHeight() {
-        return config.getHeight();
     }
 
     protected void drawBackground(Color color) {
@@ -73,37 +38,14 @@ public abstract class GraphicsApp implements GraphicsAppKeyListener, GraphicsApp
     }
 
     protected void setFrameRate(int frameRate) {
-        config.setFrameRate(frameRate);
+        getConfig().setFrameRate(frameRate);
     }
 
-    public void addObject(GraphicsObject object) {
-        objects.add(object);
+    public int getWidth() {
+        return getConfig().getWidth();
     }
 
-    public void removeObject(GraphicsObject object) {
-        objects.remove(object);
-    }
-
-    public void addToDrawBuffer(GraphicsObject object) {
-        drawBuffer.add(object);
-    }
-
-    public GraphicsObject[] getDrawBuffer() {
-        // TODO: Think about passing copies of the objects!
-        return drawBuffer.toArray(new GraphicsObject[drawBuffer.size()]);
-    }
-
-    public void clearDrawBuffer() {
-        drawBuffer.clear();
-    }
-
-    @Override
-    public void onKeyPressed(KeyPressedEvent event) {
-
-    }
-
-    @Override
-    public void onMouseClicked(MouseClickedEvent event) {
-
+    public int getHeight() {
+        return getConfig().getHeight();
     }
 }
--- src/de/mi/ur/gpr/graphics/Background.java
+++ src/de/mi/ur/gpr/graphics/Background.java
@@ -4,6 +4,7 @@ import de.mi.ur.gpr.app.GraphicsApp;
 
 public class Background extends Rectangle {
 
+    // TODO: Remove dependency to GraphicsApp by injecting correct witdth and height
     public Background() {
         super(0,0, GraphicsApp.getApp().getWidth(), GraphicsApp.getApp().getHeight());
         this.type = GraphicsObjectType.BACKGROUND;
